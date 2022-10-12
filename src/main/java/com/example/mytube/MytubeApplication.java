602ff4f177d07cfccdfe9df00461b72cf6a351ad
ohyoungkyu <come8908@gmail.com>
2022-10-12 12:49:21 +0900

    세팅

---
+++ src/main/java/com/example/mytube/MytubeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytubeApplication.class, args);
+	}
+
+}
